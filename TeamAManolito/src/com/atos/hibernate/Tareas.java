87e4404297d92fcf82d786be5b95b999ccd24f4d
Arranque 1 <Arranque 1@a201-pc02>
2018-08-07 12:02:17 +0200

    ghfv

--- TeamAManolito/src/com/atos/hibernate/Tareas.java
+++ TeamAManolito/src/com/atos/hibernate/Tareas.java
@@ -9,7 +9,25 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 public class Tareas {
+
+	private Integer codigoTarea;
+	private String tarea;
 	private Set<Roles> roles;
+
+	public Tareas() {
+		super();
+	}
+
+	public Tareas(Integer codigoTarea) {
+		super();
+		this.codigoTarea = codigoTarea;
+	}
+
+	public Tareas(Integer codigoTarea, String tarea, Set<Roles> roles) {
+		super();
+		this.codigoTarea = codigoTarea;
+		this.tarea = tarea;
+		this.roles = roles;
 	
 	// ojito aqui hay que revisar esta mierda
 	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
@@ -17,9 +35,27 @@ public class Tareas {
 	public Set<Roles> getRoles() {
 		return roles;
 	}
-
+	
 	public void setRoles(Set<Roles> roles) {
 		this.roles = roles;
 	}
-	
+
+	@id 
+	@column (name="codigo_tarea"  unique = true, nullable = false, precision = 200, scale = 0)
+	public Integer getCodigoTarea() {
+		return codigoTarea;
+	}
+
+	public void setCodigoTarea(Integer codigoTarea) {
+		this.codigoTarea = codigoTarea;
+	}
+	@column (name="tarea" nullable = false, length = 100)
+	public String getTarea() {
+		return tarea;
+	}
+
+	public void setTarea(String tarea) {
+		this.tarea = tarea;
+	}
+
 }
