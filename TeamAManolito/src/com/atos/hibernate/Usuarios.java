0db8ce2f652fc73087ddcab7405900158f878d08
DavidHinojosa <42133500+DavidHinojosa@users.noreply.github.com>
2018-08-08 11:22:36 +0200

    Version 2

--- TeamAManolito/src/com/atos/hibernate/Usuarios.java
+++ TeamAManolito/src/com/atos/hibernate/Usuarios.java
@@ -1,17 +1,13 @@
 package com.atos.hibernate;
 
-import java.util.List;
 import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
@@ -21,32 +17,22 @@ public class Usuarios {
 	private String apellidos;
 	private String correo;
 	private String password;
-	private boolean primerLogin;
-	private boolean accesoAplicacion;
+	private Integer primerLogin;
+	private Integer accesoAplicacion;
 	private Roles roles;
-	
-	
-
-	
-	
-
 
 	public Usuarios() {
 		super();
 	}
-	
-	
+
 	public Usuarios(String correo, String password) {
 		super();
 		this.correo = correo;
 		this.password = password;
 	}
-	
-	
 
-
-	public Usuarios(String nombre, String apellidos, String correo, String password, boolean primerLogin,
-			boolean accesoAplicacion, Roles roles) {
+	public Usuarios(String nombre, String apellidos, String correo, String password, Integer primerLogin,
+			Integer accesoAplicacion) {
 		super();
 		this.nombre = nombre;
 		this.apellidos = apellidos;
@@ -54,65 +40,72 @@ public class Usuarios {
 		this.password = password;
 		this.primerLogin = primerLogin;
 		this.accesoAplicacion = accesoAplicacion;
-		this.roles = roles;
+
 	}
 
-	@Column(name = "nombre", nullable = false, length = 100)
+	@Column(name = "nombre", nullable = false, length = 10)
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
-	
-	@Column(name = "apellido", nullable = false, length = 50)
+
+	@Column(name = "apellido", nullable = false, length = 10)
 	public String getApellidos() {
 		return apellidos;
 	}
+
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
-	
+
 	@Id
 	@Column(name = "correo", unique = true, nullable = false, length = 50)
 	public String getCorreo() {
 		return correo;
 	}
+
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
-	
-	@Column(name = "password", nullable = false, length = 50)
+
+	@Column(name = "password", nullable = false, length = 10)
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	@Column(name = "primer_registro", nullable = false, columnDefinition="boolean default true")
-	public boolean isPrimerLogin() {
+
+	@Column(name = "primer_registro", nullable = false, precision = 1, scale = 0)
+	public Integer getPrimerLogin() {
 		return primerLogin;
 	}
-	public void setPrimerLogin(boolean primerLogin) {
+
+	public void setPrimerLogin(Integer primerLogin) {
 		this.primerLogin = primerLogin;
 	}
-	@Column(name = "acceso", nullable = false, columnDefinition="boolean default false")
-	public boolean isAccesoAplicacion() {
+
+	@Column(name = "acceso", nullable = false, precision = 1, scale = 0)
+	public Integer getAccesoAplicacion() {
 		return accesoAplicacion;
 	}
-	public void setAccesoAplicacion(boolean accesoAplicacion) {
+
+	public void setAccesoAplicacion(Integer accesoAplicacion) {
 		this.accesoAplicacion = accesoAplicacion;
 	}
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "codigo_rol")
 	public Roles getRoles() {
 		return roles;
 	}
 
-
 	public void setRoles(Roles roles) {
 		this.roles = roles;
 	}
-	
+
 }
--- TeamAManolito/src/com/atos/hibernate/modelo/Gestion_Usuarios.java
+++ TeamAManolito/src/com/atos/hibernate/modelo/Gestion_Usuarios.java
@@ -12,26 +12,54 @@ import com.atos.hibernate.Usuarios;
 @Component("gestion_usuarios")
 @Scope("prototype")
 public class Gestion_Usuarios implements IGestion_Usuarios {
+	
 	private UsuariosDAO usuariosdao;
+	
+	private boolean credenciales;
 
 	@Override
 	@Transactional(readOnly = true)
 	public List<Usuarios> consultar_Todos() {
-		System.out.println("todos");
 		// TODO Auto-generated method stub
 		return usuariosdao.findAll();
 	}
 
 	@Override
+	@Transactional(readOnly = true)
+	// Metodo que recibe un objeto usuario en base a un string recibido (PK-correo)
 	public Usuarios consultar_correo(String correo) {
 		// TODO Auto-generated method stub
+		
 		return usuariosdao.findById(correo);
+		
 	}
 
+	@Override
+	@Transactional(readOnly = true)
+	// Metodo para comprobar si el correo y la password existe en la BBDD y devolver
+	// un true o false en su comprobacion
+	public boolean consultar_login(String correo, String password) {
+		// TODO Auto-generated method stub
+		Usuarios usu = usuariosdao.findById(correo);
+		
+		credenciales = false;
+
+		if (usu != null) {
+
+			if (usu.getCorreo().equals(correo) && usu.getPassword().equals(password)) {
+
+				credenciales = true;
+
+			}
+		} 
+		
+		return credenciales;
+	}
+
+	// ACCESORES PARA SPRING
 	public void setUsuariosdao(UsuariosDAO usuariosdao) {
 		this.usuariosdao = usuariosdao;
 	}
-	
 
-	
+
 }
